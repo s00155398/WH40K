9331ba761e4a7dfe64ee17e4ff223a91e22b4d31
Admin-HP\Gerard <gerardconnor@hotmail.com>
2020-09-29 13:51:30 +0100

    Changes to FW Aiming, Enemy.
    Hud has been added so that a crosshair is now available to improve aim
    for the player, the camera boom arm is now attached to spinal socket on
    FW to allow for smoother experience.

---
+++ Source/WH40K/TPS_HUD.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "TPS_HUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WH40K_API ATPS_HUD : public AHUD
+{
+	GENERATED_BODY()
+protected:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		UTexture2D* CrosshairTexture;
+
+public:
+	virtual void DrawHUD() override;
+	
+};
